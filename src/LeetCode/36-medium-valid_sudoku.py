9e78fc57acf309ab4ec920d5f7524107a2aafb73
James Stewart <jgstew101@gmail.com>
2023-08-25 14:28:44 -0700

    36. (Medium) Valid Sudoku

---
+++ src/LeetCode/36-medium-valid_sudoku.py
@@ -0,0 +1,92 @@
+# Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+#
+# Each row must contain the digits 1-9 without repetition.
+# Each column must contain the digits 1-9 without repetition.
+# Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+# Note:
+#
+# A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+# Only the filled cells need to be validated according to the mentioned rules.
+#
+# Input: board = 
+# [["5","3",".",".","7",".",".",".","."]
+# ,["6",".",".","1","9","5",".",".","."]
+# ,[".","9","8",".",".",".",".","6","."]
+# ,["8",".",".",".","6",".",".",".","3"]
+# ,["4",".",".","8",".","3",".",".","1"]
+# ,["7",".",".",".","2",".",".",".","6"]
+# ,[".","6",".",".",".",".","2","8","."]
+# ,[".",".",".","4","1","9",".",".","5"]
+# ,[".",".",".",".","8",".",".","7","9"]]
+# Output: true
+# Example 2:
+#
+# Input: board = 
+# [["8","3",".",".","7",".",".",".","."]
+# ,["6",".",".","1","9","5",".",".","."]
+# ,[".","9","8",".",".",".",".","6","."]
+# ,["8",".",".",".","6",".",".",".","3"]
+# ,["4",".",".","8",".","3",".",".","1"]
+# ,["7",".",".",".","2",".",".",".","6"]
+# ,[".","6",".",".",".",".","2","8","."]
+# ,[".",".",".","4","1","9",".",".","5"]
+# ,[".",".",".",".","8",".",".","7","9"]]
+# Output: false
+# Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+#  
+#
+# Constraints:
+#
+# board.length == 9
+# board[i].length == 9
+# board[i][j] is a digit 1-9 or '.'.
+
+from typing import List
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for i in range(9):
+            row = set()
+            col = set()
+            square = set()
+            for j in range(9):
+                square_start = ((i//3)*3,(i%3)*3)
+                square_item = board[square_start[0]+(j//3)][square_start[1]+(j%3)]
+                if self.not_in(board[i][j], row) and self.not_in(board[j][i], col) and self.not_in(square_item, square):
+                    row.add(board[i][j])
+                    col.add(board[j][i])
+                    square.add(square_item)
+                else:
+                    return False
+
+        return True
+
+    def not_in(self, item, collection):
+        if item == ".":
+            return True
+        else: 
+            return item not in collection
+
+solution = Solution()
+print(solution.isValidSudoku(board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]))
+
+solution = Solution()
+print(solution.isValidSudoku(board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]))
