b5ca052f7d8197ce7491e429886ed0644f79359a
James Stewart <jgstew101@gmail.com>
2023-09-13 09:42:56 -0700

    128. (Medium) Longest Consecutive Sequence

--- src/LeetCode/128-medium-longest_consecutive_sequence.py
+++ src/LeetCode/128-medium-longest_consecutive_sequence.py
@@ -21,18 +21,34 @@
 # -109 <= nums[i] <= 109
 
 """
-Idea: Use hashset! continue checking the hashset for the next greater or lesser number
+Idea: Use Set to check if next number greater than num exists in O(1). Don't scan backwards to maintain O(n) linear time. 
+
+Time: O(n) -- O(n + n) to be precise
+Space: O(n) -- from the set that we use for O(1) lookup
+
+Trick:
+    - Using set for O(1) lookup instead of iterating through array
+    - Using a less than check to make sure that we are not in the middle of a sequence, so we only scan in increasing order once
+        per consecutive sequence.
 """
 
 from typing import List
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        num_set = set(nums)
-        streak = 0
-        lesser = num_set - 1
-        while lesser
+        longest_consecutive_sequence = 0
+        nums_set = set(nums)
+        for num in nums_set:
+            if num-1 not in nums_set:
+                consecutive_sequence = 1
+                current_num = num
+                while current_num+1 in nums_set:
+                    consecutive_sequence += 1
+                    current_num += 1
+                    longest_consecutive_sequence = max(longest_consecutive_sequence, consecutive_sequence)
 
+        return longest_consecutive_sequence
 
 solution = Solution()
-print(solution.longestConsecutive([]))
+print(solution.longestConsecutive([100,4,200,1,3,2]))
+print(solution.longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
