2de9ec2e196b3536c9d39cf19006c1439f939110
James Stewart <jgstew101@gmail.com>
2023-09-13 15:35:17 -0700

    6.4 Incomplete

---
+++ src/eopi/ch6_strings/6-4_replace_and_remove.py
@@ -0,0 +1,14 @@
+""" Replace and Remove
+
+Given an input string, and an integer representing the number of characters on which to operate
+return an array of characters/string wherein all 'a's are replaced with 'dd' and all 'b's are
+removed"""
+
+"""
+Idea:
+    - Two-pass technique; first left to right pass will remove all 'b' instances with rewrite
+    - Second pass is right to left replacing all 'a' instances with 'dd'
+
+"""
+
+def replace_and_remove(chars: str, k: int) -> str:
