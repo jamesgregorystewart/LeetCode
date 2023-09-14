f1b2d9dafa82698f610f264e40e152a403087718
James Stewart <jgstew101@gmail.com>
2023-09-14 09:06:46 -0700

    python solution for 66.7

--- src/eopi/ch6_strings/6-7_look_and_say.py
+++ src/eopi/ch6_strings/6-7_look_and_say.py
@@ -43,3 +43,15 @@ print(compute_nth_look_and_say(3))
 print(compute_nth_look_and_say(4))
 print(compute_nth_look_and_say(5))
 print(compute_nth_look_and_say(6))
+
+import itertools
+
+def compute_nth_look_and_say_pythonic(n: int) -> str:
+    s = "1"
+    for _ in range (n-1):
+        s = ''.join(
+                str(len(list(group))) + key for key, group in itertools.groupby(s))
+    return s
+
+print("--------------------")
+print(compute_nth_look_and_say_pythonic(6))
