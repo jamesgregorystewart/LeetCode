05772449e8b3773980099b087e9230d89bd3df30
James Stewart <jgstew101@gmail.com>
2023-09-15 19:20:39 -0700

    7-3 Find First Node in Cycle

--- src/eopi/ch07_linked_lists/7-3_test_for_cyclicity.py
+++ src/eopi/ch07_linked_lists/7-3_test_for_cyclicity.py
@@ -8,23 +8,42 @@ Space: O(1)
 
 from util.models import *
 
-def is_list_cyclic(L: ListNode) -> bool:
+def find_start_of_cycle(L: ListNode) -> None | ListNode:
     dummy_head = ListNode(0, L)
     s_cur, f_cur = L.next, L.next.next
-
+    
+    # Find Cycle
+    intercept_node = None
     while s_cur.next and f_cur.next.next:
         if s_cur == f_cur:
-            return True
+            intercept_node = s_cur
+            break
         s_cur, f_cur = s_cur.next, f_cur.next.next
 
-    return False
+    if not intercept_node:
+        return None
+
+    # Find Cycle Length
+    s_cur, cycle_length = s_cur.next, 1
+    while s_cur != intercept_node:
+        s_cur, cycle_length = s_cur.next, cycle_length + 1
+
+    back_node = front_node = L
+    for _ in range(cycle_length):
+        front_node = front_node.next
+
+    while back_node != front_node:
+        back_node, front_node = back_node.next, front_node.next
+
+    return back_node
+
 
 L = LinkedList()
 L.append_values(list(range(20)))
 
 cycle_start = L.head
 for _ in range(4):
-    cyclee_start = cycle_start.next
+    cycle_start = cycle_start.next
 
 cycle_end = cycle_start
 while cycle_end.next:
@@ -32,9 +51,8 @@ while cycle_end.next:
 
 cycle_end.next = cycle_start
 
+print(find_start_of_cycle(L.head).data)
 
-print(is_list_cyclic(L.head))
-
-L2 = LinkedList()
-L2.append_values(list(range(20)))
-print(is_list_cyclic(L2.head))
+# L2 = LinkedList()
+# L2.append_values(list(range(20)))
+# print(find_start_of_cycle(L2.head).data)
