c8098a8973d749c5ff7ef322f55e37feb30934ff
James Stewart <jgstew101@gmail.com>
2023-09-18 20:37:24 -0700

    141 (Easy) Linked List Cycle

---
+++ src/LeetCode/141-easy_linked_list_cycly.py
@@ -0,0 +1,17 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+from typing import Optional
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast = slow = head
+        while fast and fast.next:
+            fast, slow = fast.next.next, slow.next
+            if fast == slow:
+                return True
+
+        return False
