95e1fde0375259971d71ab5dfdd031d9c1112dd2
James Stewart <jgstew101@gmail.com>
2023-12-12 10:57:33 -0800

    add a push solution

--- src/LeetCode/198-medium-house-robber.py
+++ src/LeetCode/198-medium-house-robber.py
@@ -1,12 +1,26 @@
 from typing import List
 
 
+# Pull/Standard Solution
+"""
+Here the pull is preferable because it is less vulnerable to
+index out of bounds error based on empty input
+"""
 class Solution:
     def rob(self, nums: List[int]) -> int:
         prev1, prev2 = 0, 0
         for i in range(len(nums)-1, -1, -1):
             prev1, prev2 = max(prev1, prev2 + nums[i]), prev1
         return prev1
+
+
+# Push/Forward Solution
+# class Solution:
+#     def rob(self, nums: List[int]) -> int:
+#         prev2, prev1 = nums[0], nums[1]
+#         for i in range(2, len(nums)):
+#             prev1, prev2 = max(prev1, prev2 + nums[i]), prev1
+#         return prev1
 
 
 solution = Solution()
