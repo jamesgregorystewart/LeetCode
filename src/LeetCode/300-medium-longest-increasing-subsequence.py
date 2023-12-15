78b38a2d4ee77f8bffaa29bb63f2b9b7f5788696
James Stewart <jgstew101@gmail.com>
2023-12-15 13:17:46 -0800

    300 MEDIUM Longest Increasing Subsequence Solved Twice

--- src/LeetCode/300-medium-longest-increasing-subsequence.py
+++ src/LeetCode/300-medium-longest-increasing-subsequence.py
@@ -1,17 +1,49 @@
 from typing import List
 
 
+# class Solution:
+#     def lengthOfLIS(self, nums: List[int]) -> int:
+#         dp: List[int] = [1] * len(nums)  # base case is every seq is at least 1
+#         res = 1
+#         for i in range(1, len(nums)):
+#             for j in range(0, i):
+#                 if nums[j] < nums[i]:
+#                     dp[i] = max(dp[i], dp[j]+1)
+#                 res = max(res, dp[i])
+#         return res
+
+
+# solution = Solution()
+# print(solution.lengthOfLIS(nums=[0, 1, 0, 3, 2, 3]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        dp: List[int] = [1] * len(nums)  # base case is every seq is at least 1
-        res = 1
-        for i in range(1, len(nums)):
-            for j in range(0, i):
-                if nums[j] < nums[i]:
+        dp = [1] * (len(nums)+1)
+        max_subsequence = 1
+        for i in range(len(nums)-1, -1, -1):
+            for j in range(i+1, len(nums)):
+                if nums[j] > nums[i]:
                     dp[i] = max(dp[i], dp[j]+1)
-                res = max(res, dp[i])
-        return res
+                    max_subsequence = max(max_subsequence, dp[i])
+
+        return max_subsequence
 
 
 solution = Solution()
-print(solution.lengthOfLIS(nums=[0, 1, 0, 3, 2, 3]))
+print(solution.lengthOfLIS(nums = [10,9,2,5,3,7,101,18]))
+print(solution.lengthOfLIS(nums = [0,1,0,3,2,3]))
+print(solution.lengthOfLIS(nums = [7,7,7,7,7,7,7]))
